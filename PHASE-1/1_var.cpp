f7d59c9b78fafbf192bb52ab62905441c0fb77de
bhavya porwal <bhavyaporwal007@gmail.com>
2026-06-12 17:27:10 +0530

    variables done

--- PHASE-1/1_var.cpp
+++ PHASE-1/1_var.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
-using namesspace std;
+using namespace std; // means we can use names for obj and var from the standard library
+/* 
+int = integers
+double = decimals
+char = single alphabet
+string = stores words and sentences
+bool = true and false
+*/
+
 
 int main(){
-    cout << :hello world"
-    return 0;
+    cout << "hello world" << endl;
+    cout << "new line is added" << endl; // endl print in next line
+    cout << 3 << endl;
+    cout << 3+4 << endl;
+
+
+int age = 19;
+cout << age << endl;
+
+int x=5 , y=10 , z=15;
+cout << x+y+z << endl;
+
+int a,b,c;
+a=b=c=18;
+cout << a+b+c << endl;
+    return 0; // ends the main function
+
+    // c++ also have constants which are values that cannot be changed
 }
