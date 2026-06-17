01b5927955b0a5b444b3efbbb67b06cd71370ba9
bhavya porwal <bhavyaporwal007@gmail.com>
2026-06-18 00:21:34 +0530

    pointer and memory addresses done

---
+++ PHASE-1/7_memory_things.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+     
+/*
+
+MEMORY ADDRESSES 
+- every variable is stored somewhene in the ram
+- to get address of a variable we use &
+- to read a adddress -- typical address is like 0x61ff10 -- 0x means number is written in hexadecimal and A to F refer from 10 to 15 
+
+POINTERS
+- a pointer is a variable that store the mem add of another variable
+- pointer canbe declared by this symbol * and = & at the equal to side
+- datatype *pointerName;
+- data type of pointer should match the data type of variable it is pointing to
+- can be used to modify the value of var from another function , create memory dynamically , create linked list and many more
+- but they can also crash programs and do memory leaks if not used properly
+- we can also use double pointer , stroes add of a pointer
+- can be used to change a pointer from another function
+- can be used to create dynamic 2d arrays
+
+*/
+
+
+
+    return 0;
+}
